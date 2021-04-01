6e38d0db1c4cb7bec2463eb15aaac71ca1867789
Akarsh Shukla <akarshshukla19@yahoo.com>
2021-04-01 02:33:56 +0000

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
+let random = Math.floor(Math.random() * 31) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
 // Write your code here
